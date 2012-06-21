6d611a99b32270cea43bd70fde19157dd20b1bd8
mauricio lopez <mlopez@dixian.info>
2012-06-20 20:58:07 -0500

    agregado metodo publico y lógica interna para validad RUC de sociedades  privadas.

--- ValidarIdentificacion.php
+++ ValidarIdentificacion.php
@@ -35,8 +35,8 @@
  */
 
 /**
- * ValidarIdentificacion contiene metodos para validar cédula, RUC de persona natural/juridica y
- * RUC de empresas públicas en el Ecuador.
+ * ValidarIdentificacion contiene metodos para validar cédula, RUC de persona natural, RUC de sociedad privada y
+ * RUC de socieda pública en el Ecuador.
  */
 class ValidarIdentificacion
 {
@@ -46,11 +46,6 @@ class ValidarIdentificacion
      * @var string
      */
     protected $error = '';
-
-    function __construct()
-    {
-        // nothing.
-    }
 
     /**
      * Validar cédula
@@ -71,7 +66,7 @@ class ValidarIdentificacion
         try {
             $this->validarInicial($numero, '10');
             $this->validarCodigoProvincia(substr($numero, 0, 2));
-            $this->validarTercerDigito($numero[2]);
+            $this->validarTercerDigito($numero[2], 'cedula');
             $this->algoritmoModulo10(substr($numero, 0, 9), $numero[9]);
         } catch (Exception $e) {
             $this->setError($e->getMessage());
@@ -100,9 +95,40 @@ class ValidarIdentificacion
         try {
             $this->validarInicial($numero, '13');
             $this->validarCodigoProvincia(substr($numero, 0, 2));
-            $this->validarTercerDigito($numero[2]);
+            $this->validarTercerDigito($numero[2], 'ruc_natural');
             $this->validarCodigoEstablecimiento(substr($numero, 10, 3));
             $this->algoritmoModulo10(substr($numero, 0, 9), $numero[9]);
+        } catch (Exception $e) {
+            $this->setError($e->getMessage());
+            return false; 
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Validar RUC sociedad privada
+     * 
+     * @param  string  $value  Número de RUC sociedad privada
+     * 
+     * @return Boolean
+     */
+    public function validarRucSociedadPrivada($numero = '')
+    {
+        // fuerzo parametro de entrada a string
+        $numero = (string)$numero;
+
+        // borro por si acaso errores de llamadas anteriores.
+        $this->setError('');
+
+        // validaciones
+        try {
+            $this->validarInicial($numero, '13');
+            $this->validarCodigoProvincia(substr($numero, 0, 2));
+            $this->validarTercerDigito($numero[2], 'ruc_privada');
+            $this->validarCodigoEstablecimiento(substr($numero, 10, 3));
+            $this->algoritmoModulo11(substr($numero, 0, 9), $numero[9]);
         } catch (Exception $e) {
             $this->setError($e->getMessage());
             return false; 
@@ -159,19 +185,43 @@ class ValidarIdentificacion
     
     /**
      * Validación de tercer dígito
+     *
+     * Permite validad el tercer dígito del documento. Dependiendo
+     * del campo tipo (tipo de identificación) se realizan las validaciones. 
+     * Los posibles valores del campo tipo son: cedula, ruc_natural, ruc_privada
+     *
+     * Para Cédulas y RUC de personas naturales el terder dígito debe
+     * estar entre 0 y 5 (0,1,2,3,4,5)
+     *
+     * Para RUC de sociedades privadas el terder dígito debe ser 
+     * igual a 9.
      * 
      * @param  string $numero  tercer dígito de CI/RUC
+     * @param  string $tipo  tipo de identificador
      * 
      * @return boolean
      *
-     * @throws exception Cuando el tercer digito no es 0,1,2,3,4,5
+     * @throws exception Cuando el tercer digito no es válido. El mensaje
+     * de error depende del tipo de Idenficiación.
      */
-    protected function validarTercerDigito($numero)
+    protected function validarTercerDigito($numero, $tipo)
     {
-        if ($numero < 0 OR $numero > 5) {
-            throw new Exception('Tercer dígito debe ser mayor o igual a 0 y menor a 6');
+        switch ($tipo) {
+            case 'cedula':
+            case 'ruc_natural':
+                if ($numero < 0 OR $numero > 5) {
+                    throw new Exception('Tercer dígito debe ser mayor o igual a 0 y menor a 6 para cédulas y RUC de persona natural');
+                }
+                break;
+            case 'ruc_privada':
+                if ($numero != 9) {
+                    throw new Exception('Tercer dígito debe ser igual a 9 para sociedades privadas');
+                }
+                break;
+            default:
+                throw new Exception('Tipo de Identificacion no existe.');
+                break;
         }
-
         return true;
     }
 
@@ -258,6 +308,69 @@ class ValidarIdentificacion
             $resultado = 0;        
         } else {
             $resultado = 10 - $residuo;
+        }
+
+        if ($resultado != $digitoVerificador) {
+            throw new Exception('Dígitos iniciales no validan contra Dígito Idenficador');
+        }
+
+        return true;
+    }
+
+    /**
+     * Algoritmo Modulo11 para validar RUC de sociedades privadas y públicas
+     *
+     * Los coeficientes usados para verificar el décimo dígito de la cédula, 
+     * mediante el algoritmo “Módulo 11” son:  4. 3. 2. 7. 6. 5. 4. 3. 2.
+     *
+     * Paso 1: Multiplicar cada dígito de los digitosIniciales por su respectivo 
+     * coeficiente.
+     * 
+     *  Ejemplo
+     *  digitosIniciales posicion 1  x 4
+     *  digitosIniciales posicion 2  x 3
+     *  digitosIniciales posicion 3  x 2
+     *  digitosIniciales posicion 4  x 7
+     *  digitosIniciales posicion 5  x 6
+     *  digitosIniciales posicion 6  x 5
+     *  digitosIniciales posicion 7  x 4
+     *  digitosIniciales posicion 8  x 3
+     *  digitosIniciales posicion 9  x 2
+     *
+     * Paso 2: Se suman los resultados y se obtiene total
+     *
+     * Paso 3: Divido total para 11, se guarda residuo. Se resta 11 menos el residuo.
+     * El valor obtenido debe concordar con el digitoVerificador
+     *
+     * Nota: Cuando el residuo es cero(0) el dígito verificador debe ser 0.
+     * 
+     * @param  string $digitosIniciales   Nueve primeros dígitos de RUC
+     * @param  string $digitoVerificador  Décimo dígito de RUC
+     * 
+     * @return boolean
+     *
+     * @throws exception Cuando los digitosIniciales no concuerdan contra
+     * el código verificador.
+     */
+    protected function algoritmoModulo11($digitosIniciales, $digitoVerificador)
+    {
+        $arrayCoeficientes = array(4, 3, 2, 7, 6, 5, 4, 3, 2);
+
+        $digitoVerificador = (int)$digitoVerificador;
+        $digitosIniciales = str_split($digitosIniciales);
+
+        $total = 0;
+        foreach ($digitosIniciales as $key => $value) {
+            $valorPosicion = ( (int)$value * $arrayCoeficientes[$key] );
+            $total = $total + $valorPosicion;
+        }
+
+        $residuo =  $total % 11;
+
+        if ($residuo == 0) {
+            $resultado = 0;        
+        } else {
+            $resultado = 11 - $residuo;
         }
 
         if ($resultado != $digitoVerificador) {
--- ValidarIdentificacionTest.php
+++ ValidarIdentificacionTest.php
@@ -48,7 +48,7 @@ class ValidarIdenfiticacionTest extends PHPUnit_Framework_TestCase
         // revisar tercer digito, debe ser mayor/igual a 0 y menor a 6
         $validarCedula = $this->validador->validarCedula('0996687856');
         $this->assertEquals($validarCedula, false);
-        $this->assertEquals($this->validador->getError(), 'Tercer dígito debe ser mayor o igual a 0 y menor a 6');
+        $this->assertEquals($this->validador->getError(), 'Tercer dígito debe ser mayor o igual a 0 y menor a 6 para cédulas y RUC de persona natural');
 
         // cedula incorrecta de acuerdo a algoritmo modulo10
         $validarCedula = $this->validador->validarCedula('0926687858');
@@ -104,7 +104,7 @@ class ValidarIdenfiticacionTest extends PHPUnit_Framework_TestCase
         // revisar tercer digito, debe ser mayor/igual a 0 y menor a 6
         $validarRucPersonaNatural = $this->validador->validarRucPersonaNatural('0996687856001');
         $this->assertEquals($validarRucPersonaNatural, false);
-        $this->assertEquals($this->validador->getError(), 'Tercer dígito debe ser mayor o igual a 0 y menor a 6');
+        $this->assertEquals($this->validador->getError(), 'Tercer dígito debe ser mayor o igual a 0 y menor a 6 para cédulas y RUC de persona natural');
 
         // revisar que codigo de establecimiento (3 últimos dígitos) no sean menores a 1.
         $validarRucPersonaNatural = $this->validador->validarRucPersonaNatural('0926687856000');
@@ -125,6 +125,63 @@ class ValidarIdenfiticacionTest extends PHPUnit_Framework_TestCase
         
         $validarRucPersonaNatural = $this->validador->validarRucPersonaNatural('0910005917001');
         $this->assertEquals($validarRucPersonaNatural, true);
+    }
+
+
+    public function testRucSociedadPrivada()
+    {
+        // parametro vacio o sin parametro (numero ci) deben dar false
+        $validarRucSociedadPrivada = $this->validador->validarRucSociedadPrivada('');
+        $this->assertEquals($validarRucSociedadPrivada, false);
+        $this->assertEquals($this->validador->getError(), 'Valor no puede estar vacio');
+        
+        $validarRucSociedadPrivada = $this->validador->validarRucSociedadPrivada();
+        $this->assertEquals($validarRucSociedadPrivada, false);
+        $this->assertEquals($this->validador->getError(), 'Valor no puede estar vacio');
+
+        // parametro con 0 adelante pero como integer, debe dar false ya que php lo convierte a 0
+        $validarRucSociedadPrivada = $this->validador->validarRucSociedadPrivada(0992397535001);
+        $this->assertEquals($validarRucSociedadPrivada, false);
+        $this->assertEquals($this->validador->getError(), 'Valor no puede estar vacio');
+
+        // parametro debe tener solo digitos
+        $validarRucSociedadPrivada = $this->validador->validarRucSociedadPrivada('-0992397535001');
+        $this->assertEquals($validarRucSociedadPrivada, false);
+        $this->assertEquals($this->validador->getError(), 'Valor ingresado solo puede tener dígitos');
+
+        $validarRucSociedadPrivada = $this->validador->validarRucSociedadPrivada('099,2397535001');
+        $this->assertEquals($validarRucSociedadPrivada, false);
+        $this->assertEquals($this->validador->getError(), 'Valor ingresado solo puede tener dígitos');
+
+        // ruc de sociedad privada debe tener 13 caracteres exactos
+        $validarRucSociedadPrivada = $this->validador->validarRucSociedadPrivada('0992397535001998');
+        $this->assertEquals($validarRucSociedadPrivada, false);
+        $this->assertEquals($this->validador->getError(), 'Valor ingresado debe tener 13 caracteres');
+
+        // revisar codigo de provincia, debe estar entre 0 y 24
+        $validarRucSociedadPrivada = $this->validador->validarRucSociedadPrivada('9992397535001');
+        $this->assertEquals($validarRucSociedadPrivada, false);
+        $this->assertEquals($this->validador->getError(), 'Codigo de Provincia (dos primeros dígitos) no deben ser mayor a 24 ni menores a 0');
+
+        // revisar tercer digito, debe ser igual a 9
+        $validarRucSociedadPrivada = $this->validador->validarRucSociedadPrivada('0982397535001');
+        $this->assertEquals($validarRucSociedadPrivada, false);
+        $this->assertEquals($this->validador->getError(), 'Tercer dígito debe ser igual a 9 para sociedades privadas');
+
+        // revisar que codigo de establecimiento (3 últimos dígitos) no sean menores a 1.
+        $validarRucSociedadPrivada = $this->validador->validarRucSociedadPrivada('0992397535000');
+        $this->assertEquals($validarRucSociedadPrivada, false);
+        $this->assertEquals($this->validador->getError(), 'Código de establecimiento no puede ser 0');
+
+        // ruc sociedad privada incorrecto de acuerdo a algoritmo modulo11
+        $validarRucSociedadPrivada = $this->validador->validarRucSociedadPrivada('0992397532001');
+        $this->assertEquals($validarRucSociedadPrivada, false);
+        $this->assertEquals($this->validador->getError(), 'Dígitos iniciales no validan contra Dígito Idenficador');
+    
+        // revisar que ruc correcto valide
+        $validarRucSociedadPrivada = $this->validador->validarRucSociedadPrivada('0992397535001');
+        $this->assertEquals($validarRucSociedadPrivada, true);
+        
     }
 }
 ?>
